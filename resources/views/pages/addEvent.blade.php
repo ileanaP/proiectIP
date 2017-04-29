d5e949024679c8acaebb5a48b876e266c085b593
IleanaP <ileana.profeanu@gmail.com>
2017-04-29 12:06:41 +0300

    add event blade - in progress

---
+++ resources/views/pages/addEvent.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.form')
+@section('content')
+
+    <div class="top-content">
+
+        <div class="inner-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-sm-offset-3 form-box">
+                        <div class="form-top">
+                            <div class="form-top-left">
+                                <h3>Adauga un nou eveniment</h3>
+                            </div>
+                            <div class="form-top-right">
+                                <i class="fa fa-lock"></i>
+                            </div>
+                        </div>
+                        <div class="form-bottom">
+                            <form role="form" action="{{ route('addEvent') }}" method="POST" class="login-form">
+                                {{ csrf_field() }}
+
+                                <div class="form-group">
+                                    <label class="sr-only" for="form-username">Titlu eveniment:</label>
+                                    <input type="text" name="titleEvent" id="titleEvent" placeholder="Titlul evenimentului..." class="form-username form-control">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="sr-only" for="form-password">Descriere eveniment</label>
+                                    <textarea name="descriptionEvent" id="descriptionEvent" placeholder="Descrierea evenimentului..." class="form-username form-control">
+                                    </textarea>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="sr-only" for="form-username">Categorie:</label>
+                                    <input type="text" name="category" id="category" placeholder="Categorie..." class="form-username form-control">
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label class="sr-only" for="form-username">Adresa:</label>
+                                    <input type="text" name="address" id="address" placeholder="Adresa..." class="form-username form-control">
+                                </div>
+                                <button type="submit" class="btn btn-primary">
+                                    Submit
+                                </button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <!-- Javascript -->
+    <script src="../resources/assetsLogin/js/jquery-1.11.1.min.js"></script>
+    <script src="../resources/assetsLogin/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../resources/assetsLogin/js/jquery.backstretch.min.js"></script>
+    <script src="../resources/assetsLogin/js/scripts.js"></script>
+
+    <!--[if lt IE 10]>
+    <script src="../resources/assetsLogin/js/placeholder.js"></script>
+    <![endif]-->
+
+@stop
