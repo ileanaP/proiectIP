0348076ed7c00845a626a7412cd8ebf77fe2d377
DiiaanaD <drajneanu_diana@yahoo.com>
2017-04-30 17:40:18 +0300

    fix format code

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -14,10 +14,10 @@ class UserProfileController extends Controller
 
     public function main(Request $request)
     {
-        $user_id = $request->query('id');
-        $user = User::where('id',$user_id)->get();
-        $location = Location::where('id',$user[0]->location)->get();
-        $locations = Location::where('id','>',0)->get();
+        $userId = $request->query('id');
+        $user = User::where('id', $userId)->get();
+        $location = Location::where('id', $user[0]->location)->get();
+        $locations = Location::where('id', '>', 0)->get();
 
         return view('pages.profile', compact('user','location', 'locations'));
     }
