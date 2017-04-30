19b679fb8685738220bfc1acd2b57044c9e64917
DiiaanaD <drajneanu_diana@yahoo.com>
2017-04-30 17:07:27 +0300

    fix header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -13,8 +13,6 @@
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li id="home"><a href="/">Home</a></li>
-                <li><a id="register" href="/register">Inregistrare</a></li>
-                <li><a id="login" href="/login">Autentificare</a></li>
                 <li><a id="upcomingEvents" href="/upcomingEvents">Evenimente</a></li>
                 @if (Auth::check())
                     @if (in_array(Auth::user()->id, $orgIds))
@@ -23,55 +21,13 @@
                 @endif
                 <li><a id="profile" href="/profile">Profil</a></li>
 
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="../navbar/">Default</a></li>
-                <li><!--<form id="logout-form" method="post"><a href="{{ route('logout') }}" onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">Logout</a></form>-->
-                    <a href="logout">Logout</a></li>
-                <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+
+                <li><a id="register" href="/register">Inregistrare</a></li>
+                <li><a id="login" href="/login">Autentificare</a></li>
+                <li><a href="logout">Logout</a></li>
             </ul>
         </div><!--/.nav-collapse -->
     </div>
 </nav>
-
-{{--<nav class="navbar navbar-default" role="navigation">--}}
-    {{--<div class="container">--}}
-        {{--<!-- Brand and toggle get grouped for better mobile display -->--}}
-        {{--<div class="navbar-header">--}}
-            {{--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">--}}
-                {{--<span class="sr-only">Toggle navigation</span>--}}
-                {{--<span class="icon-bar"></span>--}}
-                {{--<span class="icon-bar"></span>--}}
-                {{--<span class="icon-bar"></span>--}}
-            {{--</button>--}}
-            {{--<div class="navbar-brand navbar-brand-centered">Brand</div>--}}
-        {{--</div>--}}
-
-        {{--<!-- Collect the nav links, forms, and other content for toggling -->--}}
-        {{--<div class="collapse navbar-collapse" id="navbar-brand-centered">--}}
-            {{--<ul class="nav navbar-nav">--}}
-                {{--<li><a href="#">Link</a></li>--}}
-                {{--<li><a href="#">Link</a></li>--}}
-                {{--<li><a href="#">Link</a></li>--}}
-            {{--</ul>--}}
-            {{--<ul class="nav navbar-nav navbar-right">--}}
-                {{--<li><a href="#">Link</a></li>--}}
-                {{--<li><a href="#">Link</a></li>--}}
-                {{--<li><a href="#">Link</a></li>--}}
-            {{--</ul>--}}
-        {{--</div><!-- /.navbar-collapse -->--}}
-    {{--</div><!-- /.container-fluid -->--}}
-{{--</nav>--}}
