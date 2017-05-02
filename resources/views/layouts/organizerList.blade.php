fcb171891c853a4eb1090241631673e787d510eb
DiiaanaD <drajneanu_diana@yahoo.com>
2017-05-02 21:27:33 +0300

    add title for the list of organizers

--- resources/views/layouts/organizerList.blade.php
+++ resources/views/layouts/organizerList.blade.php
@@ -2,7 +2,7 @@
     <div class="list-group">
         <form role="form" action="{{ route('deleteOrganizers') }}" method="POST">
             {{ csrf_field() }}
-
+            <h3>Organizatori actuali: </h3>
             @foreach($org as $organizer)
                 <input type="text" class="list-group-item" value="{{$organizer->name}}" readonly><input type="checkbox" name="org_{{$organizer->id}}" value="{{$organizer->id}}">Sterge
             @endforeach
