4ac15c3e92b611f9cd96e2ffd16bea9a7527bad1
IleanaP <ileana.profeanu@gmail.com>
2017-05-25 09:44:20 +0300

    404 page refactoring - deleted noMenu layout

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,22 +1,27 @@
-@extends('layouts.noMenu')
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="error-template">
-                <h1>
-                    Oops!</h1>
-                <h2>
-                    404 Not Found</h2>
-                <div class="error-details">
-                    Sorry, an error has occured, Requested page not found!
-                </div>
-                <div class="error-actions">
-                    <a href="/" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
-                        Take Me Home </a>
+<!DOCTYPE HTML>
+<html>
+<head>
+    @include('includes.head')
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="error-template">
+                    <h1>
+                        Oops!</h1>
+                    <h2>
+                        404 Not Found</h2>
+                    <div class="error-details">
+                        Sorry, an error has occured, Requested page not found!
+                    </div>
+                    <div class="error-actions">
+                        <a href="/" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
+                            Take Me Home </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@stop
+</body>
+</html>
